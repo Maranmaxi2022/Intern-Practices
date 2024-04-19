873595505f63aa6403f4a1154693a697a5841082
Thamilmaran Mohanarasa <thamilmaranm.20@itfac.mrt.ac.lk>
2024-04-19 17:40:32 +0530

    change the path (.pkl & .csv files) in penguins-app.py

--- Penguins/penguins-app.py
+++ Penguins/penguins-app.py
@@ -4,6 +4,7 @@ import numpy as np
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 
+# Defining the Streamlit App
 st.write("""
 # Penguin Prediction App
 
@@ -12,6 +13,7 @@ This app predicts the **Palmer Penguin** species!
 Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
 """)
 
+# Sidebar for User Input
 st.sidebar.header('User Input Features')
 
 st.sidebar.markdown("""
@@ -24,22 +26,23 @@ if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
 else:
     def user_input_features():
+        # User Input Widgets
         island = st.sidebar.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
         sex = st.sidebar.selectbox('Sex', ('male', 'female'))
         bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1, 59.6, 43.9)
         bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
         flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
         body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+        # Creating Data Dictionary
         data = {'island': island,
                 'bill_length_mm': bill_length_mm,
                 'bill_depth_mm': bill_depth_mm,
                 'flipper_length_mm': flipper_length_mm,
                 'body_mass_g': body_mass_g,
                 'sex': sex}
+        # Creating DataFrame from User Input
         features = pd.DataFrame(data, index=[0])
         return features
-
-
     input_df = user_input_features()
 
 # Combines user input features with entire penguins dataset
